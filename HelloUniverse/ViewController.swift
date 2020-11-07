18533fdd3531c0e28ef6d25244d3f741d960b868
Eric Brito <eric_brito@msn.com>
2020-11-07 14:46:45 -0300

    Initial Commit

---
+++ HelloUniverse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloUniverse
+//
+//  Created by Eric Alves Brito on 07/11/20.
+//  Copyright © 2020 Afrodev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
